48a4f16bed054e15225e61d4632c0a05fae6b76a
Sachin Gupta <saching@microsoft.com>
2019-02-18 16:34:56 -0800

    making class todo app stateful

--- src/class-app/App.tsx
+++ src/class-app/App.tsx
@@ -9,17 +9,30 @@ interface IHooksAppProps {
   todos: ITodo[];
 }
 
-export class ClassTodoApp extends React.Component<IHooksAppProps, {}> {
+interface IHooksAppState {
+  todos: ITodo[];
+}
+
+export class ClassTodoApp extends React.Component<IHooksAppProps, IHooksAppState> {
   constructor(props: IHooksAppProps) {
      super(props);
+     this.state = {
+      todos: this.props.todos
+    }
+  }
+
+  private addTodo(todoText: string) {
+     const todo: ITodo = {text: todoText};
+     const newTodos = [...this.state.todos, todo];
+     this.setState({todos: newTodos});
   }
 
   render() {
-    const { todos } = this.props;
+    const { todos } = this.state;
     return (
       <div className={Styles.app}>
         <TodoListRenderer todos={todos} />
-        <TodoFormContainer />
+        <TodoFormContainer addTodo={this.addTodo.bind(this)} />
       </div>
     );
   }
--- src/class-app/TodoFormContainer.tsx
+++ src/class-app/TodoFormContainer.tsx
@@ -2,10 +2,40 @@ import * as React from "react";
 
 import { TodoForm } from "../components/TodoForm";
 
-export class TodoFormContainer extends React.Component<{}, {}> {
+interface ITodoFormContainerProps {
+  addTodo: (text: string) => {};
+}
+
+interface ITodoFormContainerState {
+  value: string;
+}
+export class TodoFormContainer extends React.Component<ITodoFormContainerProps, ITodoFormContainerState> {
+  constructor(props: ITodoFormContainerProps) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+  }
+
+  private onChange(e: any): void {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  private handleSubmit(e: Event) {
+    e.preventDefault();
+    if (this.state.value) {
+      this.props.addTodo(this.state.value);
+      this.setState({
+        value: ''
+      });
+    }
+  }
+
   render() {
     return (
-      <TodoForm />
-      );
-    }
+      <TodoForm value={this.state.value} onSubmit={this.handleSubmit.bind(this)} onChange={this.onChange.bind(this)} />
+    );
+  }
 }
