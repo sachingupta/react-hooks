7a7e85b0505fd84fb06a23c497134f0d18f03273
Sachin Gupta <saching@microsoft.com>
2019-02-23 17:53:00 -0800

    compponent using context consumer canbe function

--- src/class-app/ThemedButton.tsx
+++ src/class-app/ThemedButton.tsx
@@ -1,20 +1,15 @@
 import * as React from "react";
 import { ThemeContext } from "../components/ThemeContext";
 
-export class ThemedButton extends React.Component<{}, {}> {
-    constructor(props: {}) {
-        super(props)
-    }
-    render() {
-        return (<ThemeContext.Consumer>
-            {({ theme, toggleTheme }) => (
-                <button
-                    onClick={toggleTheme}
-                    style={{ backgroundColor: theme.background,  color: theme.color }}>
-                    Toggle Theme
+export function ThemedButton() {
+    return (<ThemeContext.Consumer>
+        {({ theme, toggleTheme }) => (
+            <button
+                onClick={toggleTheme}
+                style={{ backgroundColor: theme.background, color: theme.color }}>
+                Toggle Theme
                 </button>
-            )}
-        </ThemeContext.Consumer>
-        );
-    }
+        )}
+    </ThemeContext.Consumer>
+    );
 }
